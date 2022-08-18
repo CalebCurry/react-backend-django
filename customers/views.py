779dd8c6a8a6c7e1d7ccef7dbe24aa68069cb948
Caleb <caleb@calebcurry.com>
2022-08-18 09:26:12 -0400

    details api endpoint

--- customers/views.py
+++ customers/views.py
@@ -7,3 +7,7 @@ def customers(request):
     serializer = CustomerSerializer(data, many=True)
     return JsonResponse({'customers': serializer.data})
     
+def customer(request, id):
+    data = Customer.objects.get(pk=id)
+    serializer = CustomerSerializer(data)
+    return JsonResponse({'customer': serializer.data})
