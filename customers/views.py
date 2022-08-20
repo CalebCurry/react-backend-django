25c9a99cc08af4627becb6b35e75cfe43b4451cf
Caleb <caleb@calebcurry.com>
2022-08-20 03:39:27 -0400

    full crud

--- customers/views.py
+++ customers/views.py
@@ -1,16 +1,40 @@
 from customers.models import Customer
 from django.http import JsonResponse, Http404
 from customers.serializers import CustomerSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
+@api_view(['GET', 'POST'])
 def customers(request):
-    data = Customer.objects.all()
-    serializer = CustomerSerializer(data, many=True)
-    return JsonResponse({'customers': serializer.data})
+    if request.method == 'GET':
+        data = Customer.objects.all()
+        serializer = CustomerSerializer(data, many=True)
+        return Response({'customers': serializer.data})
     
+    elif request.method == 'POST':
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'customer' : serializer.data}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'POST', 'DELETE'])    
 def customer(request, id):
     try:
         data = Customer.objects.get(pk=id)
     except Customer.DoesNotExist:
-        raise Http404('Customer does not exist')
-    serializer = CustomerSerializer(data)
-    return JsonResponse({'customer': serializer.data})
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = CustomerSerializer(data)
+        return Response({'customer': serializer.data})
+    elif request.method == 'DELETE':
+        data.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'POST':
+        serializer = CustomerSerializer(data, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'customer': serializer.data})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
