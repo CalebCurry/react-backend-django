627a4b702f65ace4e6079a113dc7cc075eba2a26
Caleb <caleb@calebcurry.com>
2022-09-27 10:34:33 -0400

    more schema work

--- customers/models.py
+++ customers/models.py
@@ -11,7 +11,7 @@ class Customer(models.Model):
 class Order(models.Model):
     customer = models.ForeignKey(Customer, related_name="orders", on_delete=models.CASCADE)
     description = models.CharField(max_length=500)
-    totalInCents = models.IntegerField()
+    total_in_cents = models.IntegerField()
 
     def __str__(self):
         return self.description
--- customers/schema.py
+++ customers/schema.py
@@ -25,6 +25,19 @@ class CreateCustomer(graphene.Mutation):
         customer.save()
         return CreateCustomer(customer=customer)
 
+class CreateOrder(graphene.Mutation):
+    class Arguments:
+        description = graphene.String()
+        total_in_cents = graphene.Int()
+        customer = graphene.ID()
+
+    order = graphene.Field(OrderType)
+
+    def mutate(root, info, description, total_in_cents, customer):
+        order = Order(description=description, total_in_cents=total_in_cents, customer_id=customer)
+        order.save()
+        return CreateOrder(order=order)
+
 class Query(graphene.ObjectType):
     customers = graphene.List(CustomerType)
     orders = graphene.List(OrderType)
@@ -43,6 +56,7 @@ class Query(graphene.ObjectType):
             return None
 
 class Mutations(graphene.ObjectType):
-    createCustomer = CreateCustomer.Field()
+    create_customer = CreateCustomer.Field()
+    create_order = CreateOrder.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutations)
